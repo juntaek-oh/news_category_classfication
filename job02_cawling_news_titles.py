27120b298191061c80343c57577c64d50432ca2d
HyunBeen96 <kimm0620@naver.com>
2025-04-16 17:15:08 +0900

    test....

--- job02_cawling_news_titles.py
+++ job02_cawling_news_titles.py
@@ -4,7 +4,13 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-#assa
+import pandas as pd
+import datetime
+
+category = ['Politiccs', 'Economic', 'Social', 'Culture', 'World', 'IT']
+df_titles = pd.DataFrame()
+
+
 options = ChromeOptions()
 
 options.add_argument('lang=ko_KR')
@@ -12,29 +18,31 @@ options.add_argument('lang=ko_KR')
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
-url = 'https://news.naver.com/section/100'
+url = 'https://news.naver.com/section/100' # 100 수정
 driver.get(url)
-
-button_xpath = '//*[@id="newsct"]/div[4]/div/div[2]'
+titles = []
+button_xpath = '//*[@id="newsct"]/div[4]/div/div[2]' # div[4]/div/div[2] 수정
 for i in range(5):
     time.sleep(0.5)
-    driver.find_element(By.XPATH, button_xpath).click()
+    try:
+        driver.find_element(By.XPATH, button_xpath).click()
+    except:
+        print(i)
 time.sleep(5)
-
 
 for i in range(1, 5):
     for j in range(1, 7):
-        title_path = '//*[@id="newsct"]/div[4]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(i, j)
+        title_path = '//*[@id="newsct"]/div[4]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(i, j)  # div[4]/div/div[1]/ 수정
         try:
             title = driver.find_element(By.XPATH, title_path).text
+            titles.append(title)
             print(title)
         except:
             print('error', i, j)
 
-
-
-'//*[@id="newsct"]/div[4]/div/div[1]/div[1]/ul/li[5]/div/div/div[2]/a/strong'
-'//*[@id="newsct"]/div[4]/div/div[1]/div[1]/ul/li[6]/div/div/div[2]/a/strong'
-'//*[@id="newsct"]/div[4]/div/div[1]/div[2]/ul/li[6]/div/div/div[2]/a/strong'
-'//*[@id="newsct"]/div[4]/div/div[1]/div[4]/ul/li[1]/div/div/div[2]/a/strong'
-''
+df_section_titles = pd.DataFrame(titles, columns=['titles'])
+df_section_titles['category'] = category[0]     #category[0] 수정
+df_titles = pd.concat([df_titles, df_section_titles],
+                      axis='rows', ignore_index=True)
+df_titles.to_csv('./crawling_data/naver_headline_Politiccs_{}.csv'.format(
+    datetime.datetime.now().strftime('%y%m%d')), index=False) #Politiccs수정
